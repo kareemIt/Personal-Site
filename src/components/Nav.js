62ef9afe16390b7eca9467ca99667f3bacf25232
kareemIt <kareem.itani2@gmail.com>
2024-02-03 18:09:43 -0500

    css updates

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ const Nav = () => {
   return <div className='nav'>
     <ul>
       <li><a href="default.asp">Kareem Itani</a></li>
-    <div>
+    <div className='right-nav'>
       <li><a href="default.asp">Home</a></li>
       <li><a href="news.asp">About</a></li>
       <li><a href="contact.asp">Skills</a></li>
