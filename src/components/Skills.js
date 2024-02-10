851b9b0edf6cd38075f0a55179da7ea0ea7b1fba
kareemIt <kareem.itani2@gmail.com>
2024-02-10 02:15:43 -0500

    displayed all of the svgs

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import angular from '../icons/angular.svg'
+import azure from '../icons/azure.svg'
+import css from '../icons/css.svg'
+import figma from '../icons/figma.svg'
+import html from '../icons/html.svg'
+import java from '../icons/java.svg'
+import javascript from '../icons/javascript.svg'
+import node from '../icons/node.svg'
+import postgresSQL from '../icons/postgreSQL.svg'
+import react from '../icons/react.svg'
+import spring from '../icons/spring.svg'
+import typescript from '../icons/typescript.svg'
 
 const Skills = () => {
-  let skills = ["Javascript","java", "html"];
-  return <div>
-    <h1 className='parent-container'>Skills</h1>
+  const [skills,setSkills] = useState(
+    [ [java,"Java"],[javascript,"Javascript"], 
+    [typescript,"Typescript"],[react,"React"],
+    [angular,"Angular"],[css,"CSS"],
+    [html,"HTML"],[figma,"Figma"],
+    [azure,"Azure"],[node,"Node"],
+    [postgresSQL,"PostgresSQL"],[spring,"Spring"]
+  ]
+  );
+  return <div> 
+    <h1 className='title'>Skills</h1>
     <div className='skills'>
-      <p>hi</p>
-
+    {skills.map((skill) => (
+          <div>
+            <img src={skill[0]} className='icon'/>
+            <p>{skill[1]}</p>
+          </div>
+          ))}
     </div>
-    {skills.map((skill) => {
-      <div className={skill}/>
-})}
   </div>;
 };
 export default Skills;
