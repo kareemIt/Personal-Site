4641a2d2882f2a3e52750ffd8a063b7584928396
kareemIt <kareem.itani2@gmail.com>
2024-02-18 13:10:58 -0500

    removed email icon

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import github from '../icons/github.svg'
-import email from '../icons/email.svg'
 import linkedin from '../icons/linkedin.svg'
 
 
@@ -11,9 +10,6 @@ const About = () => {
       <p>
 Hey, I'm Kareem Itani, a graduate with a Bachelors of Science in Computer Science from Brooklyn College, City University of New York, class of 2022. My journey in the tech realm has shaped my commitment to making a positive impact. I'm deeply passionate about contributing to the community through my work in software engineering. Whether it's tackling challenges or delving into new technologies, I find joy in the world of coding. Outside of the digital realm, you'll likely find me exploring new hobbies and embracing the endless possibilities that come with learning. Let's connect and build a tech-inspired future together!</p>
       <div className='about-icons'>
-      <a href="https://www.w3schools.com/">
-      <img src={email}  className='button'/> 
-      </a>
       <a href="https://github.com/kareemIt">
       <img src={github}  className='button'/> 
       </a>
