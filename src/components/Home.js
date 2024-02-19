f76c4aae0d6e2c1a6f2ef76b817f9af49a5d708b
kareemIt <kareem.itani2@gmail.com>
2024-02-18 21:36:17 -0500

    jake said so

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,24 @@ import React, { useState, useEffect } from 'react';
 
 const Home = () => {
   let traits = [
-    "Software Engineer","Bouldering enjoyer",
-    "Friendly neighborhood developer","UX Enchanter & UI Charmer"
+    "Software Engineer", "Bouldering enjoyer",
+    "Friendly neighborhood developer", "UX Enchanter & UI Charmer"
   ];
   const [trait, setTrait] = useState(traits[0]);
   const [counter, setCounter] = useState(1);
   const wait = ms => new Promise(res => setTimeout(res, ms));
-
+  // show trait
+    // add class or just change opacity in react style
+  // wait some time
+    // await wait function
+  // fade out trait
+    // remove class or just change opacity in react style
+  // update trait
+    // some javascript
+  // fade in trait
+    // add class or just change opacity in react style
+  // repeat
+    // making use of setInterval 
   useEffect(() => {
     if (counter >= 4) setCounter(0);
 
@@ -34,42 +45,42 @@ const Home = () => {
         <h1 className='name'>Kareem Itani</h1>
         <h1 id='trait' className='fade-in-image'>{trait}</h1>
       </div>
-        <svg
-          className="waves"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          viewBox="0 24 150 28"
-          preserveAspectRatio="none"
-          shapeRendering="auto"
-        >
-          <defs>
-            <path
-              id="gentle-wave"
-              d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
-            />
-          </defs>
-          <g className="parallax">
-            <use
-              xlinkHref="#gentle-wave"
-              x="48"
-              y="0"
-              fill="rgba(255,255,255,0.7"
-            />
-            <use
-              xlinkHref="#gentle-wave"
-              x="48"
-              y="3"
-              fill="rgba(255,255,255,0.5)"
-            />
-            <use
-              xlinkHref="#gentle-wave"
-              x="48"
-              y="5"
-              fill="rgba(255,255,255,0.3)"
-            />
-            <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
-          </g>
-        </svg>
+      <svg
+        className="waves"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 24 150 28"
+        preserveAspectRatio="none"
+        shapeRendering="auto"
+      >
+        <defs>
+          <path
+            id="gentle-wave"
+            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+          />
+        </defs>
+        <g className="parallax">
+          <use
+            xlinkHref="#gentle-wave"
+            x="48"
+            y="0"
+            fill="rgba(255,255,255,0.7)"
+          />
+          <use
+            xlinkHref="#gentle-wave"
+            x="48"
+            y="3"
+            fill="rgba(255,255,255,0.5)"
+          />
+          <use
+            xlinkHref="#gentle-wave"
+            x="48"
+            y="5"
+            fill="rgba(255,255,255,0.3)"
+          />
+          <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
+        </g>
+      </svg>
     </div>
   );
 };
