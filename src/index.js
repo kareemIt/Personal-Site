81967d30351d56d5e954da5c4c888e70e4a76d91
kareemIt <kareem.itani2@gmail.com>
2024-02-24 17:21:00 -0500

    moved index back

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './src/App';
+import App from './App';
 
 const root = createRoot(document.getElementById('app'));
 
